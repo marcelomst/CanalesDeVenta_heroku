02b86a6ded800ff52a39a32e745e29001a01587d
Marcelo Martinez <marcelomst1@gmail.com>
2019-02-16 15:51:15 -0300

    Demo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,15 @@
 class WelcomeController < ApplicationController
   # GET /welcome
+  require 'C:\canalesDeVenta\wired'
+  require 'date'
   def index
+    wired = Wired.new
+    wired.aquire_token
+    ancillary= 1
+    mark= 0
+        
+   @response = wired.fetch_new_bookings(ancillary, mark)
+  
 
   end
 
