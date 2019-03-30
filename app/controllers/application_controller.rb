8ee35d8437d9a5b9ceb34d1dcec386eafc15b0cc
Marcelo Martinez <marcelomst1@gmail.com>
2019-03-30 16:24:20 -0300

    Demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def set_mailer_host
+    if Rails.env.development?
+      subdomain = current_account ? "#{current_account.subdomain}." : ""
+      ActionMailer::Base.default_url_options[:host] = "#{subdomain}lvh.me:5000"
+    else
+      subdomain = current_account ? "#{current_account.subdomain}." : ""
+      ActionMailer::Base.default_url_options[:host] = "#{subdomain}canalesdeventa.herokuapp.com"
+    end
+  end 
 end
